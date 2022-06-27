b4f252789556bd301db4c1012d860568406d3045
nkzren <renan.nakazawa0@gmail.com>
2022-06-27 10:09:04 -0300

    adiciona fetch de busca de url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
   const url_atual = tabs[0].url;
   // use `url` here inside the callback because it's asynchronous!
   const gov = url_atual.includes(".gov.br");
@@ -10,38 +10,32 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     document.querySelector(".camponome").style.display = "none";
     document.querySelector(".campourl").style.display = "none";
 
-    //esses dois são eu tentando fazer o botão funcionar. tenso. (problema)
-    const botao = document.querySelector(".botao");
-    botao.addEventListener("click", function () {
-      chrome.tabs.update(tabs[0].id, { url: url_atual });
-    });
+    const response = await fetch("http://localhost:3000/url?" + new URLSearchParams({domain: cleanUrlInput(url_atual)}))
+    const data = await response.json()
+
+    if (data.url) {
+      document.querySelector(".cadastro").style.display = "none";
+      //esses dois são eu tentando fazer o botão funcionar. tenso. (problema)
+      const botao = document.querySelector(".botao");
+      botao.addEventListener("click", function () {
+        chrome.tabs.create({ url: 'https://' + data.url });
+      });
+    } else {
+      document.querySelector(".pagina").style.display = "none";
+      document.querySelector(".camponome").style.display = "flex";
+      document.querySelector(".campourl").style.display = "flex";
+    }
 
     const botaoCad = document.querySelector(".botaoCad");
     botaoCad.addEventListener("click", function () {
       const nomeCidade = document.querySelector("#nomecidade").value;
       const url = document.querySelector("#url").value;
     });
-
-    //isso é pra dar fetch na API. como eu odeio coisa assíncrona aaaaaaaaaaaaaaaaaaaa (problema)
-    let valor;
-    const get = fetch("localhost:3000", {
-      body: url_atual,
-      headers: { "Content-Type": "application/x-www-form-urlencodded" },
-      method: "GET",
-    }).then((resposta) => {
-      if (resposta.ok) {
-        valor = 200;
-      } else valor = 500;
-    });
-
-    if (!(get instanceof Promise)) {
-      document.querySelector(".cadastro").style.display = "none";
-    } else {
-      document.querySelector(".pagina").style.display = "none";
-      document.querySelector(".camponome").style.display = "flex";
-      document.querySelector(".campourl").style.display = "flex";
-    }
   } else {
     document.querySelector(".siteGov").style.display = "none";
   }
 });
+
+const cleanUrlInput = (url) => {
+    return url.replace(/^https?:\/\//, '').replace(/\//, '');
+}
